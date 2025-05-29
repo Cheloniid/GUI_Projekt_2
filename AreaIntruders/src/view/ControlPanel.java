2ae5ed22d3f19cc2fd321c298e8b389297b14338
Cheloniid <61048208+Cheloniid@users.noreply.github.com>
2025-05-29 18:26:34 +0200

    initial look established

--- AreaIntruders/src/view/ControlPanel.java
+++ AreaIntruders/src/view/ControlPanel.java
@@ -1,6 +1,9 @@
 package view;
 
+import utils.Constants;
+
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,19 +13,31 @@ public class ControlPanel extends JPanel{
         private ArrayList<JButton> buttons;
 
         public ControlPanel(){
+            setBackground(Constants.GAME_PANEL_BACKGROUND);
             setLayout(new GridBagLayout());
             GridBagConstraints gbc = new GridBagConstraints();
 
             buttons = new ArrayList<>();
 
-            JButton upButton = new JButton("Up");
+            JButton upButton = new JButton("UP");
             buttons.add(upButton);
-            JButton downButton = new JButton("Down");
+            JButton downButton = new JButton("DOWN");
             buttons.add(downButton);
-            JButton rightButton = new JButton ("Right");
+            JButton rightButton = new JButton ("RIGHT");
             buttons.add(rightButton);
-            JButton leftButton = new JButton("Left");
+            JButton leftButton = new JButton("LEFT");
             buttons.add(leftButton);
+            JButton fireButton = new JButton ("FIRE!");
+            buttons.add(fireButton);
+
+            for (JButton button : buttons){
+                button.setBackground(Constants.GAME_PANEL_BACKGROUND);
+                button.setForeground(Color.GREEN);
+                button.setPreferredSize(new Dimension(80, 30));
+                //button.setBorderPainted(false);
+                //button.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 2));
+
+            }
 
             for (JButton button : buttons) {
                 button.addActionListener(new ActionListener() {
@@ -36,7 +51,7 @@ public class ControlPanel extends JPanel{
             /// /////First row
             gbc.gridy = 0;
 
-            gbc.gridwidth = 2;
+            gbc.gridwidth = 3;
             gbc.gridx = 0;
             add(upButton, gbc);
 
@@ -47,12 +62,15 @@ public class ControlPanel extends JPanel{
             add(leftButton, gbc);
 
             gbc.gridx=1;
+            add(fireButton, gbc);
+
+            gbc.gridx=2;
             add(rightButton, gbc);
 
             /// Third row
             gbc.gridy++;
             gbc.gridx = 0;
-            gbc.gridwidth = 2;
+            gbc.gridwidth = 3;
 
             add(downButton, gbc);
         }
